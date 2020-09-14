fc5eca078ba4843663dcc4e7e8b8a9da40f2503e
Robson Osorio <robsonosorio@GX2-135.gx2.local>
2020-09-14 09:17:01 -0300

    teste query params e route params

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require('express')
 
 const server = express()
 
-server.get('/registro', (req, res) =>{
+
+
+// Query params = ?registros=1
+server.get('/registros', (req, res) =>{
+    const nome = req.query.nome;
+
     return res.json({
         'registroponto': {
             'colaborador': {
                 'matricula':'1122',
-                'nome':'Charlie Brown'
+                'nome':`${nome}`,
+                'email': 'robson.osorio@gmail.com'
             },
             'registrohora': {
                 'data':'13/09/2020',
@@ -16,7 +22,27 @@ server.get('/registro', (req, res) =>{
                 'indicador':'Entrada'
             }
         }})
-
 })
+
+// Route params = /registros/1
+server.get('/registros/:id', (req, res) =>{
+    const id = req.params.id;
+
+    return res.json({
+        'registroponto': {
+            'colaborador': {
+                'Id':`testando usuario: ${id}`,
+                'nome':'Charlie Brown',
+                'email': 'robson.osorio@gmail.com'
+            },
+            'registrohora': {
+                'data':'13/09/2020',
+                'hora':'18:30',
+                'indicador':'Entrada'
+            }
+        }})
+})
+
+
 
 server.listen(3000) 
